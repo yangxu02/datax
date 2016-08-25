55ce8d7730b9c44edea48e5d7715d4a4b38c8dd8
yangxu02 <ulyx.yang@gmail.com>
2016-08-25 12:56:18 +0800

    Update DBFactory.py

--- lib/DBFactory.py
+++ lib/DBFactory.py
@@ -5,14 +5,7 @@ import MySQLdb
 from lib import Logger
 
 db_configs = {
-    'dsp' : {'host':'dsp.cgs2bjzqxcxl.us-east-1.rds.amazonaws.com','port':3306,'user':'dspadmin',"passwd":'Yna4o4AM0ljDAweZSYjJ','charset':'utf8','db':'dsp'},
-    'dsp-slave' : {'host':'dspslave.cgs2bjzqxcxl.us-east-1.rds.amazonaws.com','port':3306,'user':'dspadmin',"passwd":'Yna4o4AM0ljDAweZSYjJ','charset':'utf8','db':'dsp'},
-    'dspreport' : {'host':'ec2-54-165-200-194.compute-1.amazonaws.com','port':3306,'user':'dsp_report',"passwd":'123456','charset':'utf8','db':'dsp_report'},
-    'ssp' : {'host':'dsp.cgs2bjzqxcxl.us-east-1.rds.amazonaws.com','port':3306,'user':'sspadmin',"passwd":'pAOTpxASnoRT','charset':'utf8','db':'ssp'},
-    'ssp-slave' : {'host':'dspslave.cgs2bjzqxcxl.us-east-1.rds.amazonaws.com','port':3306,'user':'sspadmin',"passwd":'pAOTpxASnoRT','charset':'utf8','db':'ssp'},
-    'ssp-report' : {'host':'native-report.cgs2bjzqxcxl.us-east-1.rds.amazonaws.com','port':3306,'user':'sspadmin',"passwd":'pAOTpxASnoRT','charset':'utf8','db':'ssp'},
-    'ssp-report-slave' : {'host':'native-report-slave.cgs2bjzqxcxl.us-east-1.rds.amazonaws.com','port':3306,'user':'sspadmin',"passwd":'pAOTpxASnoRT','charset':'utf8','db':'ssp'},
-    'mana-slave' : {'host':'native-mana.cgs2bjzqxcxl.us-east-1.rds.amazonaws.com','port':3306,'user':'dev',"passwd":'t6W3dx8s5Dh4fmvR','charset':'utf8','db':'mana'},
+    'test' : {'host':'test.com','port':3306,'user':'testuser',"passwd":'testpwd','charset':'utf8','db':'test'},
 }
 
 db_instances = {}
